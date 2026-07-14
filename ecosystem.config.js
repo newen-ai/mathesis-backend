e014def420127aaa1d80f5eb6271239b8afb5e25
Daniel Seidler <daniel@metapool.app>
2026-07-13 23:01:47 -0300

    fix: ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       cwd: 'dist/',
       script: 'index.js',
       restart_delay: 1000,
-      watch: 'index.js',
+      watch: 'server.js',
       out_file: 'main.log',
       error_file: 'main.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss:SSS',
