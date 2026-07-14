b2ef207778603e064584feffe83a96cc36f8dcc0
Daniel Seidler <daniel@metapool.app>
2026-07-13 22:59:54 -0300

    feat: pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps : [{
+      name: 'mathesis',
+      cwd: 'dist/',
+      script: 'index.js',
+      restart_delay: 1000,
+      watch: 'index.js',
+      out_file: 'main.log',
+      error_file: 'main.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss:SSS',
+    }],
+  };
+  
+  
