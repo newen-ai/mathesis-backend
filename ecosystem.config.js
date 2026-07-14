179bd473b862a41041c9bd93cb7e95e780845297
Daniel Seidler <daniel@metapool.app>
2026-07-13 23:16:24 -0300

    fix: ecosystem cwd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     apps : [{
       name: 'mathesis',
-      cwd: 'dist/',
-      script: 'server.js',
+      cwd: '.',
+      script: 'dist/server.js',
       restart_delay: 1000,
-      watch: 'server.js',
+      watch: 'dist/server.js',
       out_file: 'main.log',
       error_file: 'main.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss:SSS',
