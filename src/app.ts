15dd21740fb7a74477bfacda1c3d33d43a96ad27
Daniel Seidler <daniel@metapool.app>
2026-07-16 20:10:28 -0300

    fix: cors options

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ export const app = express();
 
 app.use(helmet());
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(compression());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
